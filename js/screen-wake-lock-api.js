cc09c76095f584f86014ba49630065690e0e120e
YA <9039417+yzkn@users.noreply.github.com>
2025-05-01 03:38:09 +0900

    Updates

--- js/screen-wake-lock-api.js
+++ js/screen-wake-lock-api.js
@@ -1,91 +1,25 @@
 // https://github.com/mdn/dom-examples/blob/main/screen-wake-lock-api/script.js
 
 
-// // console.clear();
-
-// status paragraph
-// // const statusElem = document.querySelector('#status p');
-// toggle button
-// // const wakeButton = document.querySelector('[data-status]');
-// checkbox
-// // const reaquireCheck = document.querySelector('#reaquire');
-
-// change button and status if wakelock becomes aquired or is released
-// // const changeUI = (status = 'acquired') => {
-// //     const acquired = status === 'acquired' ? true : false;
-// //     wakeButton.dataset.status = acquired ? 'on' : 'off';
-// //     wakeButton.textContent = `Turn Wake Lock ${acquired ? 'OFF' : 'ON'}`;
-// //     statusElem.textContent = `Wake Lock ${acquired ? 'is active!' : 'has been released.'}`;
-// // }
-
-// test support
-// // let isSupported = false;
-
 if ('wakeLock' in navigator) {
-    console.log('wakeLock' in navigator); // //
-    // //     isSupported = true;
-    // //     statusElem.textContent = 'Screen Wake Lock API supported 🎉';
-    // // } else {
-    // //     wakeButton.disabled = true;
-    // //     statusElem.textContent = 'Wake lock is not supported by this browser.';
-    // // }
+    console.log('wakeLock' in navigator);
 
-    // // if (isSupported) {
-    // create a reference for the wake lock
     let wakeLock = null;
 
     // create an async function to request a wake lock
     const requestWakeLock = async () => {
         try {
             wakeLock = await navigator.wakeLock.request('screen');
-            console.log(wakeLock); // //
-
-            // change up our interface to reflect wake lock active
-            // // changeUI();
-
-            // listen for our release event
-            wakeLock.onrelease = function (ev) {
-                console.log(ev);
-            }
-            wakeLock.addEventListener('release', () => {
-                // if wake lock is released alter the button accordingly
-                // // changeUI('released');
-            });
-
+            console.log(wakeLock);
         } catch (err) {
-            // if wake lock request fails - usually system related, such as battery
-            // // wakeButton.dataset.status = 'off';
-            // // wakeButton.textContent = 'Turn Wake Lock ON';
-            // // statusElem.textContent = `${err.name}, ${err.message}`;
-            console.error(err); // //
+            console.error(err);
         }
-    } // requestWakeLock()
+    }
+    requestWakeLock();
 
-    // if we click our button
-    // // wakeButton.addEventListener('click', () => {
-    // //     // if wakelock is off request it
-    // //     if (wakeButton.dataset.status === 'off') {
-    requestWakeLock()
-    // //     } else { // if it's on release it
-    // //         wakeLock.release()
-    // //             .then(() => {
-    // //                 wakeLock = null;
-    // //             })
-    // //     }
-    // // })
-
-    const handleVisibilityChange = () => {
+    document.addEventListener('visibilitychange', _ => {
         if (wakeLock !== null && document.visibilityState === 'visible') {
             requestWakeLock();
         }
-    }
-
-    // // reaquireCheck.addEventListener('change', () => {
-    // //     if (reaquireCheck.checked) {
-    document.addEventListener('visibilitychange', handleVisibilityChange);
-    // //     } else {
-    // //         document.removeEventListener('visibilitychange', handleVisibilityChange);
-    // //     }
-    // // });
-
-} // isSupported
+    });
+}
